c083b4555cd4666c33b0b8093231a3d6898fea07
IbrahemAtef <bulbul.20001@gmail.com>
2020-12-18 07:37:34 +0200

    working on landing page

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -1,91 +1,90 @@
-import React from 'react'
-import axios from 'axios'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
 
 class SignUp extends React.Component {
   state = {
     userName: '',
     email: '',
     password: '',
-  }
+    type: ''
+  };
 
   handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value,
-    })
+    });
   }
 
   async handleSubmit(e) {
-    e.preventDefault()
-    const { userName, email, password } = this.state
+    e.preventDefault();
+    const { userName, email, password, type } = this.state;
     try {
-      let token = await axios.post('/api/auth/loginUser', {
-        userName,
-        email,
-        password,
-      })
-      localStorage.setItem('token', token)
+      let result = await (await axios.post("/api/users/createUser", {userName, email, password, type })).data
+      localStorage.setItem('token', result.token)
     } catch (error) {
       console.log(error)
     }
   }
 
   render() {
-    ;<div className="page-main">
-      <div className="bg-layer">
-        <h1>Matbikhi</h1>
-        <div className="header-main">
-          <div className="main-icon">
-            <img
-              src="https://www9.0zz0.com/2020/11/28/19/473729025.png"
-              alt="Done"
-            />
-          </div>
-          <div className="header-left-bottom">
-            <form onClick={this.handleSubmit.bind(this)}>
-              <div className="icon1">
-                <FontAwesomeIcon icon={faEnvelope} />
-                <input
-                  type="text"
-                  name="username"
-                  placeholder="UserName"
-                  required
-                  value={this.state.userName}
-                  onChange={this.handleChange.bind(this)}
-                />
-              </div>
-              <div className="icon1">
-                <FontAwesomeIcon icon={faEnvelope} />
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email Address"
-                  required
-                  value={this.state.email}
-                  onChange={this.handleChange.bind(this)}
-                />
-              </div>
-              <div className="icon1">
-                <FontAwesomeIcon icon={faLock} />
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  required
-                  value={this.state.password}
-                  onChange={this.handleChange.bind(this)}
-                />
-              </div>
-              <div className="bottom">
-                <button className="btn">Sign Up</button>
-              </div>
-            </form>
+    return (
+      <div className='page-main'>
+        <div className='bg-layer'>
+          <h1>Matbikhi</h1>
+          <div className='header-main'>
+            <div className='main-icon'>
+              <img
+                src='https://www9.0zz0.com/2020/11/28/19/473729025.png'
+                alt='Done'
+              />
+            </div>
+            <div className='header-left-bottom'>
+              <form>
+                <div className='icon1'>
+                  <FontAwesomeIcon icon={faUser} />
+                  <input
+                    type='text'
+                    name='userName'
+                    placeholder='User Name'
+                    required
+                    value={this.state.userName}
+                    onChange={this.handleChange.bind(this)}
+                  />
+                </div>
+                <div className='icon1'>
+                  <FontAwesomeIcon icon={faEnvelope} />
+                  <input
+                    type='email'
+                    name='email'
+                    placeholder='Email Address'
+                    required
+                    value={this.state.email}
+                    onChange={this.handleChange.bind(this)}
+                  />
+                </div>
+                <div className='icon1'>
+                  <FontAwesomeIcon icon={faLock} />
+                  <input
+                    type='password'
+                    name='password'
+                    placeholder='Password'
+                    required
+                    value={this.state.password}
+                    onChange={this.handleChange.bind(this)}
+                  />
+                </div>
+                <div className='bottom' onClick={this.handleSubmit.bind(this)}>
+                  <button className='btn'>Sign Up</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    );
   }
 }
 
-export default SignUp
+export default SignUp;
---
+++ client/src/components/landing/landing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Landing extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div className='landing'>
+        {/* Start Header */}
+        
+        {/* End Header */}
+      </div>
+    );
+  }
+}
+
+export default Landing;
