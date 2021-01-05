a7b74065acf67f0f58bf63fea245fbb59f27e2ed
MohammedAlzayyan <mohammedznn1999@gmail.com>
2021-01-05 12:15:37 +0300

    Done

--- server/models/SavedRecipes.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const SaveRecipeSchema = new mongoose.Schema({
-  userID: { type: String, required: true },
-  recipeID: { type: String, required: true },
-})
-
-module.exports = mongoose.model('SaveRecipes', SaveRecipeSchema)
